c25b2362a64f685d66c4d4b998c8b4100eb968cd
Ryan Tablada <ryan.tablada@gmail.com>
2016-06-02 12:42:39 -0500

    Update todo based on user scaffold

--- web/controllers/todo_controller.ex
+++ web/controllers/todo_controller.ex
@@ -1,8 +1,13 @@
 defmodule Todo.TodoController do
   use Todo.Web, :controller
 
+  alias Todo.Task
+
   def index(conn, _params) do
-    render conn, "index.html"
+    changeset = Task.changeset(%Task{})
+
+    conn
+    |> render("index.html", changeset: changeset, action: todo_path(conn, :store))
   end
 
   def store(conn, _params) do
